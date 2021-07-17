48a0cdcb879f5f38a06376e64bbfc35f4db8e25b
Muhammad Zuhriyan Sauqi <hello@zuhriyansauqi.com>
2021-07-17 23:47:08 +0700

    Added Instagram's zoom like

--- app/src/main/java/com/zuhriyansauqi/frisianflag/MainActivity.kt
+++ app/src/main/java/com/zuhriyansauqi/frisianflag/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.zuhriyansauqi.frisianflag
 
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.zuhriyansauqi.frisianflag.databinding.ActivityMainBinding
+import com.zuhriyansauqi.frisianflag.libs.zoomhelper.ZoomHelper
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
@@ -14,5 +16,10 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        return ZoomHelper.getInstance().dispatchTouchEvent(ev!!, this)
+                || super.dispatchTouchEvent(ev)
     }
 }
